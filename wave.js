cb442283e77cb8c06a3cf4b585a4e3bd4774dd7a
TylerIsHim-coder <tylerkoh408@gmail.com>
2024-10-29 16:30:40 -0700

    updated wave.js

--- wave.js
+++ wave.js
@@ -7,5 +7,22 @@ class Wave {
         this.timer = 0;
         this.groupDelay = 60;
         this.memberDelay = 5;
+        this.currentGroup = 0;
+        this.currentMember = 0;
+    }
+
+    timeToSpawn(group, member) {
+        //How much time per group 
+        var groupDuration = this.memberDelay * (this.groupSize - 1);
+         //When a certain group will start 
+        var groupStart = group * (groupDelay + groupDuration);
+          //When a certain member will start (after its group starts) 
+        var memberStart = member * memberDelay;
+
+        if(timer == groupStart + memberStart) { 
+            return true; 
+        }else{ 
+            return false; 
+        }
     }
 }
